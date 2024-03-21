00ddc9ddfaff591a281532bf2de0757a89cf0e96
MarkMusili <musiliyrn@gmail.com>
2024-03-21 20:10:43 +0300

    Id access update

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -7,13 +7,12 @@ CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
     UPDATE users
     SET users.average_score = (
-        SELECT AVG(score) 
+        SELECT AVG(score)
         FROM corrections
         WHERE corrections.user_id = user_id
     )
-    WHERE user_id = user_id;
+    WHERE users.id = user_id;
 
 END //
 
 DELIMITER ;
-
